23bbbaee956ce595d1e401ae46abd92682fc6e95
edernatanzz <edernatan124@gmail.com>
2024-12-07 01:27:26 -0300

    feat: User e seu resource

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,83 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User(String email, long id, String name, String password, String phone) {
+        this.email = email;
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        return this.id == other.id;
+    }
+
+}
